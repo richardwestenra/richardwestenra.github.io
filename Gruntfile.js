ed7a30c11bc87a9198c0ac34f3f13a7e7a5c9503
Richard Westenra <rjwestenra@gmail.com>
2014-10-05 16:24:08 +0100

    Use master as deployment branch

--- Gruntfile.js
+++ Gruntfile.js
@@ -289,7 +289,7 @@ module.exports = function (grunt) {
       dist: {
         options: {
           remote: 'git@github.com:richardwestenra/richardwestenra.github.io.git',
-          branch: 'gh-pages',
+          branch: 'master',
           commit: true,
           push: true
         }
