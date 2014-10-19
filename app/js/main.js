2f3d6ec02ecddbfaef58b018117d7b961bb967b0
Richard Westenra <rjwestenra@gmail.com>
2014-10-19 13:48:53 +0100

    Add homepage rotating stripes and config

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,25 @@
 $(function(){
 	'use strict';
+
+	//--- Global variables ---//
+
+
+
+	//--- Helper functions ---//
+
+
+	//---  ---//
+	function Config(){
+		this.active = false;
+		this.html = $('#config_tmpl').html();
+	}
+
+
+	//--- Config popup ---//
+	var $config = $( $('#config_tmpl').html() );
+	$('body').prepend($config);
+	$('.configBtn').on('click',function(e){
+		e.preventDefault();
+		$config.slideToggle(240);
+	});
 });
